20d6c3dbf7d9d9f3da31ed450aceb5918d80473a
SungYinYang <54567577+SungYinYang@users.noreply.github.com>
2020-08-27 16:17:10 +0800

    add git ignore

--- jupiter/src/main/java/external/GitHubClient.java
+++ jupiter/src/main/java/external/GitHubClient.java
@@ -29,6 +29,7 @@ public class GitHubClient {
 	private static final String URL_TEMPLATE = "https://jobs.github.com/positions.json?description=%s&lat=%s&long=%s";
 	private static final String DEFAULT_KEYWORD = "developer";
 	
+	//search function
 	public List<Item> search(double lat, double lon, String keyword) {
 		if (keyword == null) {
 			keyword = DEFAULT_KEYWORD;
